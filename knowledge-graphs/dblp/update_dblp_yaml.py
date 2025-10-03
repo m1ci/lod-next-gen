1d8b03c373d063f888e1e3aac957957c845dedf4
Milan Dojčinovski <dojcinovski.milan@gmail.com>
2025-10-03 14:06:55 +0200

    Update update_dblp_yaml.py

--- knowledge-graphs/dblp/update_dblp_yaml.py
+++ knowledge-graphs/dblp/update_dblp_yaml.py
@@ -75,8 +75,8 @@ def update_yaml(new_date, url, data):
                 "file": url,
                 "format": "nt",
                 "compression": "gz",
-                "size": None,  # optional: can fetch Content-Length
-                "sha256": None,  # optional: calculate hash
+                "size": 4745938862,  # optional: can fetch Content-Length
+                "sha256": 6b148c103921f48a2bfa290bd1c7d86730d1a551fce63425a4dc3aa3d63c390f,  # optional: calculate hash
                 "status": "pending"
             }
         ]
